d2ed978e5b9423b0381aaf5349d8c4397ac4ca7f
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-13 16:12:48 +0300

    SidebarLogo
    init

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,10 +9,16 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <div className="h-screen bg-black">
             <div className="container h-full mx-auto xl:px-30 max-w-6xl">
                 <div className="grid grid-cols-4 h-full">
-                    <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
-                        <Sidebar />
-                        {children}
-                    </div>
+                <Sidebar />
+                <div 
+                    className="
+                    col-span-3 
+                    lg:col-span-2 
+                    border-x-[1px] 
+                    border-neutral-800"
+                    >
+                    {children}
+                </div>
                 </div>
             </div>  
         </div>
