c0d34447e869b5987986b3f134dc39b3cc296ac3
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-13 16:24:41 +0300

    Update SidebarItem.tsx
    interface

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,4 +1,18 @@
-const SidebarItem = () => {
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+    label: string;
+    icon: IconType;
+    href?: string;
+    onClick?: () => void;
+  }
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    label,
+    href,
+    icon,
+    onClick
+}) => {
     return (
         <div></div>
     );
